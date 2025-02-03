2b7d5cf3c9d7e00fdcd2fce6a05b40ab2395efe9
MDAhemadKhan1 <ahemadahemad15526@gmail.com>
2025-02-03 10:23:36 +0530

    adding

---
+++ variables/variables.tf
@@ -0,0 +1,42 @@
+variable "ami-id" {
+    type = string
+    default = "ami-09c813fb71547fc4f"
+    description = "AMI ID of RHEL9 Image"
+}
+
+variable "instance_type" {
+    # type = string
+    # default = "t3.micro"
+}
+
+variable "from_port" {
+    type = number
+    default = 22
+}
+
+variable "to_port" {
+    default = 22
+}
+
+variable "cidr-blocks" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
+
+variable "ec2-tags" {
+    type = map(string)
+    default = {
+        Project = "expense"
+        Component = "backend" 
+        Environment = "dev"
+        Name = "expense-backend-dev"
+         }
+}
+
+variable "sg-tags" {
+    type = map(string)
+    default = {
+        Name = "expense-backend-dev"
+         }
+}
+
